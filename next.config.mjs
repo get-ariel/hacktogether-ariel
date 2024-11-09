3c6bf0066e5724b57a191d1282650b0d7023145d
Pedro Henriques <pedro.carvalho.henriques@outlook.com>
2024-11-09 17:40:56 +0000

    fixed next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
+  webpack: (config, { isServer }) => {
+    config.optimization.splitChunks = {
+      chunks: "all",
+      maxInitialRequests: 25,
+      minSize: 20000,
+      maxSize: 24000000,
+      cacheGroups: {
+        default: false,
+        vendors: false,
+      },
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
