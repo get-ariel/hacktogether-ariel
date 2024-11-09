157944818057698a0f696eb28fcdcb0e26d15dea
Pedro Henriques <pedro.carvalho.henriques@outlook.com>
2024-11-09 18:20:12 +0000

    cleaned up next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,33 +2,6 @@
 const nextConfig = {
   compress: true,
   output: "export",
-  webpack: (config, { isServer }) => {
-    config.optimization = {
-      ...config.optimization,
-      splitChunks: {
-        chunks: "all",
-        maxInitialRequests: 25,
-        minSize: 20000,
-        maxSize: 24000000,
-        cacheGroups: {
-          default: false,
-          vendors: false,
-          framework: {
-            name: "framework",
-            test: /[\\/]node_modules[\\/](react|react-dom|next)[\\/]/,
-            priority: 40,
-            chunks: "all",
-          },
-          lib: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: 30,
-            chunks: "all",
-          },
-        },
-      },
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
